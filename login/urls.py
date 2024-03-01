a18cd0b042cf22326bff45d0582d576ec260238e
ronphilip25 <sanchezronphilip@gmail.com>
2024-03-01 17:07:11 +0800

    Dashboard almost done

--- login/urls.py
+++ login/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.login),
+    path('dashboard/', views.dashboard, name="dashboard"),
 ]
--- login/views.py
+++ login/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def login(request):
     return render(request, 'login/login.html')
+
+
+def dashboard(request):
+    return render(request, 'dashboard/dashboard.html')
