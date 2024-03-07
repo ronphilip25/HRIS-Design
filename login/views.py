701e710fc936626387f056369c360bad487f27a3
ronphilip25 <sanchezronphilip@gmail.com>
2024-03-07 17:37:16 +0800

    attendance board done

--- login/views.py
+++ login/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,7 +7,195 @@ def login(request):
 
 
 def dashboard(request):
-    return render(request, 'dashboard/dashboard.html')
+    context = {
+        "dashboard": [
+            {
+                "name": "Jane Nicole",
+                "department": "Teaching",
+                "tardiness": "0",
+                "absences": "0",
+            },
+            {
+                "name": "Celvin Nucum",
+                "department": "IT",
+                "tardiness": "0",
+                "absences": "0",
+            },
+            {
+                "name": "Louis Dizon",
+                "department": "IT",
+                "tardiness": "0",
+                "absences": "0",
+            },
+            {
+                "name": "Sean Owen Turner",
+                "department": "IT",
+                "tardiness": "0",
+                "absences": "0",
+            },
+            {
+                "name": "Ivan Magtite",
+                "department": "IT",
+                "tardiness": "0",
+                "absences": "0",
+            },
+            {
+                "name": "Carlo Pingol",
+                "department": "IT",
+                "tardiness": "0",
+                "absences": "0",
+            },
+            {
+                "name": "Cleavant",
+                "department": "IT",
+                "tardiness": "0",
+                "absences": "0",
+            },
+            {
+                "name": "Jim manese",
+                "department": "IT",
+                "tardiness": "0",
+                "absences": "0",
+            },
+            {
+                "name": "John Doe",
+                "department": "IT",
+                "tardiness": "0",
+                "absences": "0",
+            },
+            {
+                "name": "John Doe",
+                "department": "IT",
+                "tardiness": "0",
+                "absences": "0",
+            },
+            {
+                "name": "John Doe",
+                "department": "IT",
+                "tardiness": "0",
+                "absences": "0",
+            },
+            {
+                "name": "John Doe",
+                "department": "IT",
+                "tardiness": "0",
+                "absences": "0",
+            },
+            {
+                "name": "John Doe",
+                "department": "IT",
+                "tardiness": "0",
+                "absences": "0",
+            },
+            
+        ],
+        
+        "approvals": [
+            {
+                "name": "John Doe",
+                "department": "IT",
+                "type": "Leave",
+                "request_date": "02/24/2024",
+            },
+            {
+                "name": "John Doe",
+                "department": "IT",
+                "type": "Leave",
+                "request_date": "02/24/2024",
+            },
+            {
+                "name": "John Doe",
+                "department": "IT",
+                "type": "Leave",
+                "request_date": "02/24/2024",
+            },
+            {
+                "name": "John Doe",
+                "department": "IT",
+                "type": "Leave",
+                "request_date": "02/24/2024",
+            },
+            {
+                "name": "John Doe",
+                "department": "IT",
+                "type": "Leave",
+                "request_date": "02/24/2024",
+            },
+        ],
+    }
+    
+    return render(request, 'dashboard/dashboard.html', context=context)
 
 def attendance(request):
-    return render(request, 'attendance/attendance.html')
+    threshold = request.GET.get("threshold")
+    if threshold == "weekly":
+        return render(request, 'attendance/weekly.html')
+
+    context = {
+        "attendance": [
+            {
+                "date": "02/24/2024",
+                "day": "Tuesday",
+                "total_working_hrs": "8hrs",
+                "in_out": "9:00 AM - 6:00 PM",
+                "break": "12:00 NN - 1:00 PM",
+                "overtime": "2hrs",
+                "notes": "Hello",
+            },
+            {
+                "date": "02/24/2024",
+                "day": "Tuesday",
+                "total_working_hrs": "8hrs",
+                "in_out": "9:00 AM - 6:00 PM",
+                "break": "12:00 NN - 1:00 PM",
+                "overtime": "2hrs",
+                "notes": "Hello",
+            },
+            {
+                "date": "02/24/2024",
+                "day": "Tuesday",
+                "total_working_hrs": "8hrs",
+                "in_out": "9:00 AM - 6:00 PM",
+                "break": "12:00 NN - 1:00 PM",
+                "overtime": "2hrs",
+                "notes": "Hello",
+            },
+            {
+                "date": "02/24/2024",
+                "day": "Tuesday",
+                "total_working_hrs": "8hrs",
+                "in_out": "9:00 AM - 6:00 PM",
+                "break": "12:00 NN - 1:00 PM",
+                "overtime": "2hrs",
+                "notes": "Hello",
+            },
+            {
+                "date": "02/24/2024",
+                "day": "Tuesday",
+                "total_working_hrs": "8hrs",
+                "in_out": "9:00 AM - 6:00 PM",
+                "break": "12:00 NN - 1:00 PM",
+                "overtime": "2hrs",
+                "notes": "Hello",
+                "action": "",
+            },
+            {
+                "date": "02/24/2024",
+                "day": "Tuesday",
+                "total_working_hrs": "8hrs",
+                "in_out": "9:00 AM - 6:00 PM",
+                "break": "12:00 NN - 1:00 PM",
+                "overtime": "2hrs",
+                "notes": "Hello",
+                "action": "",
+            },
+        ],
+        
+        "user": [
+            {
+                "name": "Patricia Kim Rosauro",
+            }
+        ]
+        
+    }
+    return render(request, 'attendance/attendance.html', context=context)
